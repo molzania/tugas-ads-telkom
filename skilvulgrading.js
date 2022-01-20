f3bca69381629e65fbbd65d162d8e28c94dbba81
molzania <molzania_1507@yahoo.co.id>
2022-01-20 15:09:00 +0700

    adding skilvul grading  soal 4

--- skilvulgrading.js
+++ skilvulgrading.js
@@ -1,7 +1,10 @@
-//function cekNIlai(nilai){
-    
-  //  else(nilai<68){
-    // return nilai;
-   // }
-
-// }
+// 1. grade 73 
+// Nilai di atas dapat dilakukan ke pembulatan 75, karena jarak antara 73 ke 75 kurang dari 3. LULUS
+// 2. grade 87 
+// Nilai di atas tidak dapat dilakukan ke 85 karena nilainya kelebihan dua. LULUS
+// 3. grade = 68
+// Nilai di atas dapat dilakukan pembulatan ke 70 karena jarak antara 68 ke 70 kurang dari dua. LULUS
+// 4. grade = 67
+// Nilai di atas tidak dapat dilakukan pembulatan, karena kurang dari 68. TIDAK LULUS
+// 5 grade = 43
+// Nilai di atas tidak dapat dilakukan pembulatan, karena kurang dari 68. TIDAK LULUS
